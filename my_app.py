c64310cbc26d30d1f5fdd4b34d2f714a7a1fbb8f
leonidas678289-boop <leonidas678289@gmail.com>
2025-11-13 20:01:43 -0600

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -4,3 +4,21 @@ from PyQt5.QtWidgets import (
        QHBoxLayout, QVBoxLayout,
        QGroupBox, QRadioButton,
        QPushButton, QLabel, QListWidget, QLineEdit)
+
+from instr import *
+from second_win import *
+
+class MainWin(QWidget):
+   def __init__(self):
+       ''' la ventana en donde se encuentra el saludo  '''
+       super().__init__() 
+       self.set_appear()
+       self.initUI()
+       self.connects() 
+       self.show() 
+
+   def initUI(self):
+       ''' crea elementos gráficos '''
+       self.btn_next = QPushButton(txt_next, self)
+       self.hello_text = QLabel(txt_hello)
+       self.instruction = QLabel(txt_instruction)
