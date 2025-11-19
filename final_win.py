763e8f98722da17a641c557895fa20e868d10cf2
leonidas678289-boop <leonidas678289@gmail.com>
2025-11-18 19:38:07 -0600

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -1,1 +1,18 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
 
+
+from instr import *
+
+class FinalWin(QWidget):
+   def __init__(self):
+       ''' ventana de las respuestas  '''
+       super().__init__() 
+       self.set_appear()
+       self.initUI()
+       self.connects() 
+       self.show() 
