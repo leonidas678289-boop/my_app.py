1036fc1f7f94c7f39ea8ab264079be82c8df5786
leonidas678289-boop <leonidas678289@gmail.com>
2025-11-18 19:49:15 -0600

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -26,6 +26,10 @@ class FinalWin(QWidget):
        self.layout_line.addWidget(self.workh_text, alignment = Qt.AlignCenter)
        self.setLayout(self.layout) 
 
+   def set_appear(self):
+       self.setWindowTitle(txt_title)
+       self.resize(win_width, win_height)
+       self.move(win_x, win_y) 
 
 
 
