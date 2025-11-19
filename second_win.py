ede35d7349dd6ad07852911d7187fb0969d945bb
leonidas678289-boop <leonidas678289@gmail.com>
2025-11-18 19:12:13 -0600

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -1,1 +1,11 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+
+
+from instr import *
+from final_win import *
 
