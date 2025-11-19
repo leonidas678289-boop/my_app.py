5b51d67232733859201e771eb4c36030cfc9b953
leonidas678289-boop <leonidas678289@gmail.com>
2025-11-18 19:08:19 -0600

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -21,4 +21,35 @@ class MainWin(QWidget):
        ''' crea elementos gráficos '''
        self.btn_next = QPushButton(txt_next, self)
        self.hello_text = QLabel(txt_hello)
-       self.instruction = QLabel(txt_instruction)
+       self.instruction = QLabel(txt_instruction) 
+
+       self.layout_line = QVBoxLayout()
+       self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+       self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+       self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+       self.setLayout(self.layout_line)
+
+   def next_click(self):
+       self.tw = TestWin()
+       self.hide()
+
+   def connects(self):
+       self.btn_next.clicked.connect(self.next_click) 
+
+   def set_appear(self):
+       self.setWindowTitle(txt_title)
+       self.resize(win_width, win_height)
+       self.move(win_x, win_y) 
+
+   app = QApplication([])
+   mw = MainWin()
+   app.exec_()
+
+
+
+
+
+
+
+
+
