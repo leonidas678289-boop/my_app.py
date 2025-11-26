8509c6d1b67ee211755fe19b5696e85135a35664
leonidas678289-boop <leonidas678289@gmail.com>
2025-11-25 19:08:37 -0600

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,4 +1,5 @@
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont
 from PyQt5.QtWidgets import (
        QApplication, QWidget,
        QHBoxLayout, QVBoxLayout,
