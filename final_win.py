2159ab35944b11a829012ea62d2e9de9463cb85a
leonidas678289-boop <leonidas678289@gmail.com>
2025-11-25 19:46:58 -0600

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -1,7 +1,8 @@
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont
 from PyQt5.QtWidgets import (
        QApplication, QWidget,
-       QHBoxLayout, QVBoxLayout,
+       QHBoxLayout, QVBoxLayout, QGridLayout,
        QGroupBox, QRadioButton,
        QPushButton, QLabel, QListWidget, QLineEdit)
 
