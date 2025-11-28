d0f710968bd251c7d60e0828202072eed39d8a3a
leonidas678289-boop <leonidas678289@gmail.com>
2025-11-27 19:24:26 -0600

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -40,19 +40,50 @@ class FinalWin(QWidget):
        if self.exp.age == 9 or self.exp.age == 10:
            if self.index >= 19.5:
                return txt_res1
-           elif self.index < 19 and self.index >= 15:
+           elif self.index < 19.4 and self.index >= 15.5:
                return txt_res2
-           elif self.index < 17 and self.index >= 12:
+           elif self.index < 15.4 and self.index >= 10.5:
                return txt_res3
-           elif self.index < 12 and self.index >= 6.5:
+           elif self.index < 10.4 and self.index >= 5:
                return txt_res4
            else:
                return txt_res5
 
+       if self.exp.age == 11 or self.exp.age == 12:
+           if self.index >= 18:
+               return txt_res1
+           elif self.index < 17.9 and self.index >= 14:
+               return txt_res2
+           elif self.index < 13.9 and self.index >= 9:
+               return txt_res3
+           elif self.index < 8.9 and self.index >= 3.5:
+               return txt_res4
+           else:
+               return txt_res5
 
+       if self.exp.age == 13 or self.exp.age == 14:
+           if self.index >= 16.5:
+               return txt_res1
+           elif self.index < 16.4 and self.index >= 12.5:
+               return txt_res2
+           elif self.index < 12.4 and self.index >= 7.5:
+               return txt_res3
+           elif self.index < 7.4 and self.index >= 2:
+               return txt_res4
+           else:
+               return txt_res5
 
-
-
+       if self.exp.age >= 15:
+           if self.index >= 15:
+               return txt_res1
+           elif self.index < 14.9 and self.index >= 11:
+               return txt_res2
+           elif self.index < 10.9 and self.index >= 6:
+               return txt_res3
+           elif self.index < 5.9 and self.index >= 0.5:
+               return txt_res4
+           else:
+               return txt_res5
 
    def initUI(self):
        ''' crea elementos gráficos '''
